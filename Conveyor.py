7fe8db3f93f81f9d3754c1329c4ffb936ca502ef
Ariel Sinaga <marieleffendis@gmail.com>
2026-06-19 22:59:40 +0700

    Changes Arm and Conveyor

---
+++ Conveyor.py
@@ -0,0 +1,32 @@
+from serial.tools import list_ports
+from pydobotplus import Dobot
+
+CONVEYOR_SPEED = 1
+CONVEYOR_DELAY = 1.16
+
+def init_dobot():
+    """Mencari port dan menghubungkan ke Dobot."""
+    available_ports = list_ports.comports()
+    if not available_ports:
+        print("[ERROR] Tidak ada port serial yang ditemukan.")
+        return None
+
+    port = available_ports[1].device
+    print(f"[INFO] Mencoba terhubung ke Dobot di port: {port}...")
+    try:
+        device = Dobot(port=port)
+        print("[INFO] Dobot terhubung berhasil.")
+        return device
+    except Exception as e:
+        print(f"[ERROR] Gagal connect ke Dobot: {e}")
+        return None
+
+device = init_dobot()
+
+def start_conveyor():
+    print("[CONVEYOR] START")
+    device.conveyor_belt(speed=CONVEYOR_SPEED, direction=1)
+
+def stop_conveyor():
+    print("[CONVEYOR] STOP")
+    device.conveyor_belt(speed=0, direction=1)
